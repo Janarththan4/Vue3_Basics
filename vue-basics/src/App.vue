a915f9d83a8dd81814d474c8d83625b8b825b47f
Janarththan4 <ssjanarththan.4@gmail.com>
2022-06-04 10:45:01 -0700

    Vue project created

---
+++ vue-basics/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+
+
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  }
+}
+
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap');
+body {
+  font-family: 'Poppins', sans-serif;
+  background: rgba(0,0,0,.75);
+  text-align: center;
+  color: white;
+}
+</style>
